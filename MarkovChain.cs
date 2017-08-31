49b0fd5b41be22af864419bb850d6f7e80d6a9aa
Gustavo Dias de Oliveira <gust.dias@gmail.com>
2017-08-31 20:47:35 -0300

    Markov Chain C#

---
+++ MarkovChain.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarkovChainCSharp
+{
+    public class MarkovChain<T>
+    {
+        private double[,] m_transitionMatrix;
+        private List<T> m_states = new List<T>();
+
+        public MarkovChain(double[,] transitionMatrix, List<T> states)
+        {
+            SetChain(transitionMatrix, states);
+        }
+
+        public MarkovChain(double[,] transitionMatrix, params T[] states)
+        {
+            SetChain(transitionMatrix, states);
+        }
+
+        public int Size
+        {
+            get
+            {
+                return m_transitionMatrix.GetLength(0);
+            }
+        }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                return m_transitionMatrix[i, j];
+            }
+            set
+            {
+                m_transitionMatrix[i, j] = value;
+            }
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                return m_states[i];
+            }
+            set
+            {
+                m_states[i] = value;
+            }
+        }
+
+        public List<T> States
+        {
+            get
+            {
+                List<T> list = new List<T>();
+                list.AddRange(m_states);
+                return list;
+            }
+            set
+            {
+                int size = Size;
+                m_states.Clear();
+                for (int i = 0; i < size; i++)
+                    m_states.Add(value[i]);
+            }
+        }
+
+        public double[,] Matrix
+        {
+            get
+            {
+                int size = Size;
+                double[,] matrix = new double[size, size];
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        matrix[i, j] = m_transitionMatrix[i, j];
+                return matrix;
+            }
+            set
+            {
+                int size = Size;
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                        m_transitionMatrix[i, j] = value[i, j];
+                }
+            }
+        }
+
+        public void SetChain(double[,] transitionMatrix, IList<T> states)
+        {
+            int size = transitionMatrix.GetLength(0);
+            m_transitionMatrix = new double[size, size];
+            m_states.Clear();
+            for (int i = 0; i < size; i++)
+            {
+                m_states.Add(states[i]);
+                for (int j = 0; j < size; j++)
+                    m_transitionMatrix[i,j] = transitionMatrix[i,j];
+            }
+        }
+
+        public void SetChain(double[,] transitionMatrix, params T[] states)
+        {
+            List<T> list = new List<T>();
+            list.AddRange(states);
+            SetChain(transitionMatrix, list);
+        }
+
+        public void Normalize()
+        {
+            int size = Size;
+            for (int i = 0; i < size; i++)
+            {
+                double rowSum = 0;
+                for (int j = 0; j < size; j++)
+                    rowSum += m_transitionMatrix[i, j];
+                for (int j = 0; j < size; j++)
+                    m_transitionMatrix[i, j] /= rowSum;
+            }
+        }
+
+        public double[] GetPi(double[] p0, int timeInterval)
+        {
+            if (timeInterval < 0)
+                return null;
+            Normalize();
+            int size = Size;
+            double[] s = new double[size];
+            for (int index = 0; index < size; index++)
+                s[index] = p0[index];
+            if (timeInterval == 0)
+            {
+                return s;
+            }
+            for (int time = 0; time < timeInterval; time++)
+            {
+                double[] sNext = new double[size];
+                for (int index = 0; index < size; index++)
+                {
+                    sNext[index] = 0;
+                    for (int k = 0; k < size; k++)
+                    {
+                        sNext[index] += s[k] * m_transitionMatrix[k, index];
+                    }
+                }
+                for (int index = 0; index < size; index++)
+                    s[index] = sNext[index];
+            }
+            return s;
+        }
+
+        public double GetProbability(T initialState, int timeInterval, Func<T, bool> probabilityConditionTester)
+        {
+            double p = 0;
+            foreach (T state in m_states)
+            {
+                if (probabilityConditionTester(state))
+                    p += GetProbability(initialState, state, timeInterval);
+            }
+            return p;
+        }
+
+        public double GetProbability(double[] p0, T state, int timeInterval)
+        {
+            int stateIndex = m_states.IndexOf(state);
+            return GetPi(p0, timeInterval)[stateIndex];
+        }
+
+        public double GetProbability(T initialState, T state, int timeInterval)
+        {
+            int size = Size;
+            int initialStateIndex = m_states.IndexOf(initialState);
+            double[] initialStatesProbs = new double[size];
+            initialStatesProbs[initialStateIndex] = 1;
+            return GetProbability(initialStatesProbs, state, timeInterval);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace MarkovChainCSharp
+{
+    class Program
+    {
+        /* Here, the MarkovChain class will be tested */
+        static void Main(string[] args)
+        {
+            CalcPiTest();
+            GetProbabilityTest();
+            GetChainProbabilityTest();
+            Console.WriteLine("Allright! :D");
+            Console.ReadKey();
+        }
+
+        private static void AssertEquals(double expected, double result, double delta)
+        {
+            if (Math.Abs(expected - result) < delta)
+                return;
+            throw new Exception("Assertion fail");
+        }
+
+        private static void AssertEquals(int expected, int result)
+        {
+            if (expected == result)
+                return;
+            throw new Exception("Assertion fail");
+        }
+
+        public static void CalcPiTest()
+        {
+            MarkovChain<int> matrix = new MarkovChain<int>(
+                    new double[,] {
+                    {1.0 / 3,  2.0 / 3,  0,        0      },
+                    {1.0 / 3,  1.0 / 3,  1.0 / 3,  0      },
+                    {1.0 / 3,  1.0 / 3,  1.0 / 6,  1.0 / 6},
+                    {0,        0,        0,        1}
+                    }, 100, 200, 300, 400);
+
+            // p_2
+            double[] expectedResult = { 1.0 / 3, 4.0 / 9, 11.0 / 54, 1.0 / 54 };
+            double[] result = matrix.GetPi(
+                    new double[] { 2.0 / 3, 1.0 / 3, 0, 0 }, // p_0
+                    2);
+            AssertEquals(expectedResult.Length, result.Length);
+            for (int i = 0; i < result.Length; i++)
+                AssertEquals(expectedResult[i], result[i], 0.001);
+
+            // p_3
+            expectedResult = new double[] { 0.32716, 0.43827, 0.18210, 0.05247 };
+            result = matrix.GetPi(
+                    new double[] { 2.0 / 3, 1.0 / 3, 0, 0 }, // p_0
+                    3);
+            AssertEquals(expectedResult.Length, result.Length);
+            for (int i = 0; i < result.Length; i++)
+                AssertEquals(expectedResult[i], result[i], 0.0001);
+
+            // p_1
+            expectedResult = new double[] { 1.0 / 3, 5.0 / 9, 1.0 / 9, 0 };
+            result = matrix.GetPi(
+                    new double[] { 2.0 / 3, 1.0 / 3, 0, 0 },  // p_0
+                    1);
+            AssertEquals(expectedResult.Length, result.Length);
+            for (int i = 0; i < result.Length; i++)
+                AssertEquals(expectedResult[i], result[i], 0.0001);
+        }
+
+        public static void GetProbabilityTest()
+        {
+            MarkovChain<string> matrix = new MarkovChain<string>(
+                new double[,] {
+                    {1.0 / 3,  2.0 / 3,  0,        0      },
+                    {1.0 / 3,  1.0 / 3,  1.0 / 3,  0      },
+                    {1.0 / 3,  1.0 / 3,  1.0 / 6,  1.0 / 6},
+                    {0,        0,        0,        1}
+                    }, "A", "B", "C", "D");
+
+            double res = matrix.GetProbability(new double[] { 2.0 / 3, 1.0 / 3, 0, 0 }, "C", 2);
+            AssertEquals(11.0 / 54, res, 0.001);
+            res = matrix.GetProbability(new double[] { 2.0 / 3, 1.0 / 3, 0, 0 }, "A", 3);
+            AssertEquals(0.32716, res, 0.00001);
+            res = matrix.GetProbability(new double[] { 2.0 / 3, 1.0 / 3, 0, 0 }, "D", 1);
+            AssertEquals(0, res, 0.001);
+
+            res = matrix.GetProbability("B", "A", 2);
+            AssertEquals(1.0 / 3, res, 0.001);
+            res = matrix.GetProbability("B", "D", 3);
+            AssertEquals(9.0 / 108, res, 0.0001);
+            res = matrix.GetProbability("C", "A", 1);
+            AssertEquals(1.0 / 3, res, 0.0001);
+            res = matrix.GetProbability("C", "C", 0);
+            AssertEquals(1.0, res, 0.0001);
+            res = matrix.GetProbability("C", "D", 0);
+            AssertEquals(0.0, res, 0.0001);
+            res = matrix.GetProbability("C", "B", 0);
+            AssertEquals(0.0, res, 0.0001);
+            res = matrix.GetProbability("C", "A", 0);
+            AssertEquals(0.0, res, 0.0001);
+        }
+
+        public static void GetChainProbabilityTest()
+        {
+            MarkovChain<char> markov = new MarkovChain<char>(
+                new double[,] {
+                    {1.0 / 3,  2.0 / 3,  0,        0      },
+                    {1.0 / 3,  1.0 / 3,  1.0 / 3,  0      },
+                    {1.0 / 3,  1.0 / 3,  1.0 / 6,  1.0 / 6},
+                    {0,        0,        0,        1}
+                    }, 'A', 'B', 'C', 'D');
+
+            double res;
+            res = markov.GetProbability('B', 'A', 2);
+            AssertEquals(1.0 / 3, res, 0.0001);
+            res = markov.GetProbability('B', 'D', 3);
+            AssertEquals(9.0 / 108, res, 0.0001);
+
+            res = markov.GetProbability('C', 'A', 1);
+            AssertEquals(1.0 / 3, res, 0.0001);
+            res = markov.GetProbability('C', 'D', 1);
+            AssertEquals(1.0 / 6, res, 0.0001);
+            res = markov.GetProbability('C', 'C', 0);
+            AssertEquals(1.0, res, 0.0001);
+            res = markov.GetProbability('C', 'A', 0);
+            AssertEquals(0.0, res, 0.0001);
+            res = markov.GetProbability('C', 'B', 0);
+            AssertEquals(0.0, res, 0.0001);
+            res = markov.GetProbability('C', 'D', 0);
+            AssertEquals(0.0, res, 0.0001);
+
+            res = markov.GetProbability('C', 2, state => state > 'B');
+            AssertEquals(12.0 / 36, res, 0.0001);
+        }
+    }
+}
